9675b5e3132931066e1964803b970e5c992c8ce0
keyermann <kevin.eyermann@ign.fr>
2013-04-30 15:48:21 +0200

    Ajout de la dépendance au ElsassSeeraiwer UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new ElsassSeeraiwer\ESDemoBundle\ElsassSeeraiwerESDemoBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new ElsassSeeraiwer\ESUserBundle\ElsassSeeraiwerESUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
